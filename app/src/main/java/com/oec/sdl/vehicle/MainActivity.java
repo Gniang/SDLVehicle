f0d9cfe061d2b3717a2d95a0426eef302835640f
Gniang <jing.art@gmail.com>
2019-10-19 21:25:22 +0900

    ロック解除で鳴く

--- app/src/main/java/com/oec/sdl/vehicle/MainActivity.java
+++ app/src/main/java/com/oec/sdl/vehicle/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int soundNoTouch;
+    private SoundPool soundPool;
+    private int soundBreak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +30,54 @@ public class MainActivity extends AppCompatActivity {
         Intent update_service = new Intent(context , SdlService.class);
         startService(update_service);
 
-        UpdateReceiver  upReceiver = new UpdateReceiver ();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("UPDATE_ACTION");
-        registerReceiver(upReceiver, intentFilter);
+        // オーディオ情報準備
+        {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    // USAGE_MEDIA
+                    // USAGE_GAME
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    // CONTENT_TYPE_MUSIC
+                    // CONTENT_TYPE_SPEECH, etc.
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(audioAttributes)
+                    // ストリーム数に応じて
+                    .setMaxStreams(2)
+                    .build();
+
+            // one.wav をロードしておく
+            soundNoTouch = soundPool.load(this, R.raw.notouch, 1);
+
+            // two.wav をロードしておく
+            //soundBreak = soundPool.load(this, R.raw.two, 1);
+
+            // load が終わったか確認する場合
+            soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+                @Override
+                public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                    Log.d("debug","sampleId="+sampleId);
+                    Log.d("debug","status="+status);
+                }
+            });
+        }
+
+        // SDLサービスからのデータ受信
+        {
+            UpdateReceiver  upReceiver = new UpdateReceiver ();
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction("UPDATE_ACTION");
+            registerReceiver(upReceiver, intentFilter);
+        }
+
+        // ロック解除時
+        {
+            final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+            filter.addAction(Intent.ACTION_USER_PRESENT);
+            registerReceiver(new PhoneUnlockedReceiver(), filter);
+        }
+
 
 
         //If we are connected to a module we want to start our SdlService
@@ -56,6 +107,22 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = intent.getExtras();
             String msg = extras.getString("message");
             ((TextView)findViewById(R.id.dbgText)).setText(msg);
+        }
+    }
+
+
+    public class PhoneUnlockedReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_USER_PRESENT)){
+                Log.d("debug", "Phone unlocked");
+                // one.wav の再生
+                // play(ロードしたID, 左音量, 右音量, 優先度, ループ,再生速度)
+                soundPool.play(soundNoTouch, 1.0f, 1.0f, 0, 0, 1);
+            }else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+                Log.d("debug", "Phone locked");
+            }
         }
     }
 }
